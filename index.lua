24c93688ba197395a2bf4f4ba0fe5b0296525aba
WriteCoin <sovtsardom@gmail.com>
2020-11-05 02:42:49 +0300

    Начата заготовка

---
+++ index.lua
@@ -0,0 +1,7 @@
+-- зависимости
+require 'OtherFunctions.index'
+
+-- содержимое
+
+-- объекты РО
+compiletime = require 'OOP-New.objects.compiletime'
---
+++ objects/compiletime.lua
@@ -0,0 +1,5 @@
+---@param func function
+---@return boolean
+local function compiletime(func) end
+
+return compiletime
